b04f8febe0e74da215052da3fe8c22aab63752f4
오경환 <okyeonghwan5@gmail.com>
2021-03-25 14:23:48 +0900

    초기화 파라미터

--- ServletEx/src/com/bit/servlet/LifecycleServlet.java
+++ ServletEx/src/com/bit/servlet/LifecycleServlet.java
@@ -3,6 +3,8 @@ package com.bit.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +23,7 @@ public class LifecycleServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 요청이 들어올 때 호출, 요청 방식에 따라 doGet or doPost
+		//요청 메서드 방식에 관계 없이 호출된다
 		System.out.println("Lifecycle: service()");
 		super.service(req, resp);
 	}
@@ -29,8 +32,33 @@ public class LifecycleServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 요청이 GET 메서드일 때
 		System.out.println("Lifecycle: doGet()");
+		// 컨텍스트 파라미터 불러오기
+		// 1. 웹 앱의 컨텍스트 확보
+		ServletContext context = getServletContext();
+		String dburl = context.getInitParameter("dburl");
+		String dbuser = context.getInitParameter("dbuser");
+		String dbpass = context.getInitParameter("dbpass");
+		
+		// 서블릿 초기화 파라미터
+		// 먼저 서블릿 설정 받아오기
+		ServletConfig config = getServletConfig();
+		String servletName = config.getInitParameter("servlet-name");
+		
+		// 응답 인코딩 설정
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		
 		PrintWriter out = resp.getWriter();
 		out.println("<h1>doGet call</h1>");
+		out.println("<h3>Context Init Params</h3>");
+		out.println("<ul>");	//ul : 순서 없는 리스트
+		out.println("<li>dburl:" + dburl + "</li>");	//li : 리스트 아이템
+		out.println("<li>dbuser:" + dbuser + "</li>");
+		out.println("<li>dbpass:" + dbpass + "</li>");
+		out.println("</ul>");
+		
+		out.println("<h3>servlet Init Params</h3>");
+		out.println("<p>servlet-name: " + servletName + "</p>");
 	}
 
 	@Override
