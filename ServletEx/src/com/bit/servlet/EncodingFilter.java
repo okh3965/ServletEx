1c532cfb110ef0f57d696b097e78d8d291a4aa27
오경환 <okyeonghwan5@gmail.com>
2021-03-25 16:31:26 +0900

    Emaillist web app model 1

---
+++ ServletEx/src/com/bit/servlet/EncodingFilter.java
@@ -0,0 +1,50 @@
+package com.bit.servlet;
+
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		Filter.super.init(filterConfig);
+	}
+	
+	
+	@Override
+	public void doFilter(ServletRequest req, 
+			ServletResponse resp, 
+			FilterChain chain)
+			throws IOException, ServletException {
+		// 요청의 캐릭터 인코딩을 utf-8로 전환
+		req.setCharacterEncoding("UTF-8");	//변환 작업 수행
+
+		// 응답에 관련된 필터 처리
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		// 뒤 쪽에 연결된 필터에 요청과 응답을 전달
+		chain.doFilter(req, resp);
+		
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		Filter.super.destroy();
+	}
+
+
+
+
+
+	
+}
